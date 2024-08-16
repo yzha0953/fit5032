2bdc19ae4dc06ef26016dfb45b95c876cccfd329
unknown <yzha0953@student.monash.edu>
2024-08-17 00:38:25 +1000

    finished activity 4.1

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,13 +5,21 @@
                 <h1 class="text-center">User Information Form</h1>
                 <form @submit.prevent="submitForm">
                     <div class="row mb-3">
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6">
                             <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" v-model="formData.username">
+                            <input type="text" class="form-control" id="username"  
+                            @blur ="() => validateName(true)"
+                            @input="() => validateName(false)"
+                            v-model="formData.username" />
+                            <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 col-sm-6">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" v-model="formData.password">
+                            <input type="password" class="form-control" id="password"  
+                            @blur ="() => validatePassword(true)"
+                            @input="() => validatePassword(false)"
+                            v-model="formData.password">
+                            <div v-if="errors.password" class="text-danger">{{ errors.password}}</div>
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -23,16 +31,21 @@
                         </div>
                         <div class="col-md-6">
                             <label for="gender" class="form-label">Gender</label>
-                            <select class="form-select" id="gender" v-model="formData.gender">
+                            <select class="form-select" id="gender" 
+                            @blur ="() => validateGender(true)"
+                            @input="() => validateGender(false)"
+                            v-model="formData.gender">
                                 <option value="male">Male</option>
                                 <option value="female">Female</option>
                                 <option value="other">Other</option>
                             </select>
+                            <div v-if="errors.gender" class="text-danger">{{ errors.gender}}</div>
                         </div>
                     </div>
                     <div class="mb-3">
                         <label for="reason" class="form-label">Reason for joining</label>
-                        <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
+                        <textarea class="form-control" id="reason" rows="3" 
+                        v-model="formData.reason"></textarea>
                     </div>
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -76,26 +89,87 @@ const formData = ref({
 const submittedCards = ref([]);
 
 const submitForm = () => {
-    submittedCards.value.push({
+    validateName(true)
+    validatePassword(true)
+    validateGender(true)
+    if(!errors.value.username && !errors.value.password && !errors.value.gender){
+        submittedCards.value.push({
         ...formData.value
-    });
+    }); 
+        clearForm();
+    }
+    
 };
+
+const clearForm = () => {
+    formData.value = {
+        username: '',
+        password: '',
+        isAustralian: false,
+        reason: '',
+        gender: ''
+    };
+};
+
+const errors = ref({
+    username: null,
+    password: null,
+    resident: null,
+    gender: null,
+    reason: null
+});
+
+const validateName = (blur) => {
+    if (formData.value.username.length < 3) {
+        if (blur) errors.value.username = "Name must be at least 3 characters";
+    }
+    else { errors.value.username = null; }
+}
+
+const validatePassword = (blur) => {
+    const password = formData.value.password;
+    const minlength = 8;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if(password.length < minlength){
+        if(blur) errors.value.password = `Password must be at least ${minlength} characters long.`;
+    } else if (!hasUppercase){
+        if(blur) errors.value.password = "Password must contain at least one upercase letter.";
+    } else if (!hasLowercase){
+        if(blur) errors.value.password = "Password must contain at least one lowercase letter.";
+    } else if (!hasNumber){
+        if(blur) errors.value.password = "Password must contain at least one number.";
+    } else if (!hasSpecialChar){
+        if(blur) errors.value.password = "Password must contain at least one special character.";
+    } else{
+        errors.value.password = null;}
+}
+const validateGender = (blur) => {
+    if (!formData.value.gender) {
+        if (blur) errors.value.gender = "Gender must be selected";
+    }
+    else { errors.value.gender = null; }
+
+}
 </script>
 
 <style scoped>
 /* Our logic will go here */
-.card {
-   border: 1px solid #ccc;
-   border-radius: 10px;
-   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-   }
-.card-header {
-   background-color: #275FDA;
-   color: white;
-   padding: 10px;
-   border-radius: 10px 10px 0 0;
-   }
-.list-group-item {
-   padding: 10px;
-   }
+    .card {
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+    .card-header {
+    background-color: #275FDA;
+    color: white;
+    padding: 10px;
+    border-radius: 10px 10px 0 0;
+    }
+    .list-group-item {
+    padding: 10px;
+    }
 </style>
