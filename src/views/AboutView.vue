9f6e5371551da381375354e82373e4a809efbb02
unknown <yzha0953@student.monash.edu>
2024-10-18 22:04:34 +1100

    Trigger deployment

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>About Our Library</h1>
-    <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+    <p>Welcome to our digital library! We are dedicated to providing a vast collection of books and resources to our community.</p>
   </div>
 </template>
 
