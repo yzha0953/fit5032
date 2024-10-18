e871e952928e253c16313929bd370702880ff73c
unknown <yzha0953@student.monash.edu>
2024-10-18 23:33:40 +1100

    process environment variable again

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.MODE === 'production'
+  base: process.env.NODE_ENV  === 'production'
     ? '/fit5032/'  
     : '/', 
   plugins: [
